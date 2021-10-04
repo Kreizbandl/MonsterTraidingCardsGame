2fb1fa74185b971a92ae45a72ca11db76ead03f5
Manuel T <manuelthuer72@gmail.com>
2021-10-04 22:26:38 +0200

    Erster Versuch push

--- src/main/java/com/technikum/monstertraidingcardsgame/MonsterTraidingCardsGame.java
+++ src/main/java/com/technikum/monstertraidingcardsgame/MonsterTraidingCardsGame.java
@@ -3,5 +3,6 @@ package com.technikum.monstertraidingcardsgame;
 public class MonsterTraidingCardsGame {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd!\n");
+        System.out.println("What's happening?\n");
     }
 }
